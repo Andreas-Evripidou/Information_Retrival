7600e01dd0b7420757bea0098d36c18e92e34cf4
Andreas-Evripidou <eandreas1@sheffield.ac.uk>
2023-11-04 14:18:56 +0000

    Final version

--- my_retriever.py
+++ my_retriever.py
@@ -16,7 +16,7 @@ class Retrieve:
         # Compute the number of documents in the collection
         self.num_docs = self.compute_number_of_documents()
         # Compute the document term matrix
-        self.doc_term_matrix = self.compute_doc_term_matrix()
+        self.document_vectors_length = self.compute_document_vectors_length()
         
     
     def compute_number_of_documents(self): 
@@ -25,6 +25,22 @@ class Retrieve:
             self.doc_ids.update(self.index[term])
         return len(self.doc_ids)
 
+    # Calculates the length of each document vector (the square
+    # root of the sum of the term weights squared)
+    # returns a dictionary of document ids and their length
+    # document_vectors_length = {doc_id: length}
+    def compute_document_vectors_length(self):
+        document_vectors_length = {}
+        for term in self.index.keys():
+            for doc in self.index[term]:
+                if doc in document_vectors_length:
+                    document_vectors_length[doc] += self.get_weighting(term, doc) ** 2
+                else:
+                    document_vectors_length[doc] = self.get_weighting(term, doc) ** 2
+        for doc in document_vectors_length.keys():
+            document_vectors_length[doc] = math.sqrt(document_vectors_length[doc])
+        return document_vectors_length
+    
     #==============================================================================
     # Query Processing
 
@@ -32,107 +48,78 @@ class Retrieve:
     # represented as a list of preprocessed terms). ​Returns list 
     # of doc ids for relevant docs (in rank order).
     def for_query(self, query):
-        matrix = self.doc_term_matrix
-        query_vector = self.compute_query_vector(query)
-        scores = {}
+
+        # Filter query to only include known terms
+        query = [term for term in query if term in self.index.keys()]
+
+        # Filter the documents to only include the ones that contain 
+        # at least one of the query terms
+        relevant_docs = set()
+        relevant_docs = [doc for term in query for doc in self.index[term]] 
         
-        for doc_id in matrix:
-            scores[doc_id] = self.cosine_similarity(query_vector, matrix[doc_id])
+        # Compute the query vector and the document vectors
+        query_vector, document_vectors = self.compute_vectors(query)
+        
+        # Compute the cosine similarity between the query and each document
+        scores_dict = {}
+        for doc_id in relevant_docs:
+            scores_dict[doc_id] = self.cosine_similarity(query_vector, document_vectors[doc_id], doc_id)
 
-        ranked_docs = self.rank_documents(scores)
-
-        return ranked_docs
+        # Rank the documents based on the cosine similarity
+        ranked_docs = self.rank_documents(scores_dict)
+        return ranked_docs[:10]
     
-    #==============================================================================
-    # Rank Results
-
     # Method ranking the documents based on the cosine similarity
     # Returns a list of document ids in rank order
     def rank_documents(self, cosine_simularity: dict):
-        return sorted(cosine_simularity, key= cosine_simularity.get,  reverse=True)[:10]
-
-    # Method performing binary comperation for a single query
-    # Returns a m x n document-term matrix where:a
-    # m is the number of documents in the collection
-    # n is the number of terms in the query
-    # The values in the matrix are the frequency of the term in the document
-    # For each vector, we also compute the denominator for the cosine similarity
-    def compute_doc_term_matrix(self):
-        # Initialise the document-term matrix
-        doc_term_matrix = {}
-       
-        # For each document in the collection
-        for doc_id in self.doc_ids:
-
-            # Initialise the document vector    
-            doc = {}
-            # Initialise the denominator for the cosine similarity
-            doc['ENUMERATOR'] = 0
-           
-            # For each term in the query
-            for term in self.index:
-                # If the term is in the document, add the frequency
-                if doc_id in self.index[term]:
-                    doc[term] = self.index[term][doc_id] * self.get_weighting(term, doc_id)
-                    # Compute the denominator for the cosine similarity
-                    doc['ENUMERATOR'] += doc[term] **  2
-                
-                # If the term is not in the document, add 0
-                else:
-                    doc[term] = 0
-            
-            # Compute the denominator for the cosine similarity
-            doc['DENOMINATOR'] = math.sqrt(doc['ENUMERATOR'])
-            # Add the document vector to the document-term matrix
-            doc_term_matrix[doc_id] = doc
-        return doc_term_matrix
+        return sorted(cosine_simularity, key= cosine_simularity.get,  reverse=True)
     
+    #==============================================================================
+    # Vector Space Model
 
-
-    # Method for finding the term frequency for a single query
-    # Returns t vectore of term frequencies:
-    def compute_query_vector(self, query: list):
-        # Initialise the query set and the query vector
-        query_set = set(query)
+    # Method for computing the query and document vectors
+    # Returns a dictionary of the query vector and a dictionary of the document vectors
+    # document_vectors = {doc_id: {term: weight}}
+    # query_vector = {term: weight}
+    def compute_vectors(self, query: list, relevant_docs: list):
+        # Initialise the query vector and the document vectors
+        document_vectors = {}
         query_vector = {}
 
         # For each term in the query
+        query_set = set(query)
         for term in query_set:
-            # If the term is not in the index, continue
-            # (Skipping unkown terms)
+            # If the term is not in the index, continue (Skipping unkown terms)
             if term not in self.index:
                 continue
 
-            # If the term is not in the query vector, add it
-            if query_vector.get(term) == None:
-                query_vector[term] = self.get_query_weighting(term, query)
+            query_vector[term] = self.get_query_weighting(term, query)
 
-            # Else, increment the frequency
-            else:
-                query_vector[term] += self.get_query_weighting(term, query)
+            relevant_docs = self.index[term]
+            for doc in relevant_docs:
+                if doc not in document_vectors:
+                    document_vectors[doc] = {}
+                document_vectors[doc][term] = self.get_weighting(term, doc)
 
-        # Return the query vector
-        return query_vector
+        # Return the query vector and the document vectors
+        return query_vector, document_vectors
     
     #==============================================================================
     # Compare Query and Document
-
         
     # Method for finding the cosine similarity between a query and a document
     # Returns the cosine similarity between the two vectors
-    def cosine_similarity(self, query_vec: dict , doc_vec: dict):
-
+    def cosine_similarity(self, query_vector: dict , doc_vector: dict, doc_id: int):
         # Initialise the numerator
         numerator = 0
-        query_denominator = 0
 
         # Compute the numerator
-        for term in query_vec:
-            numerator += query_vec[term] * doc_vec[term]
-            query_denominator += query_vec[term] ** 2
+        for term in query_vector:
+            if term in doc_vector:
+                numerator += query_vector[term] * doc_vector[term]
         
         # Get the denominator from the document vector
-        denominator = doc_vec['DENOMINATOR']
+        denominator = self.document_vectors_length[doc_id]
 
         # Return the cosine similarity
         return numerator / denominator
@@ -140,12 +127,15 @@ class Retrieve:
     #==============================================================================
     # Weighting Schemes
 
+    # Method for computing the query tf term weighting
     def compute_query_tf(self, term: str, query: list):
         return query.count(term)
     
+    # Method for computing the query tfidf term weighting
     def compute_query_tfidf(self, term: str, query: list):
        return self.compute_query_tf(term, query) * self.compute_idf(term)
     
+    # Method for deciding the query term weighting
     def get_query_weighting(self, term: str, query: list):
         if self.term_weighting == "binary":
             return 1
@@ -153,16 +143,20 @@ class Retrieve:
             return self.compute_query_tf(term, query)
         elif self.term_weighting == 'tfidf':
             return self.compute_query_tfidf(term, query)
-                
+
+    # Method for computing the document tf term weighting      
     def compute_tf(self, term: str, doc_id: int):
         return self.index[term][doc_id]
-      
+
+    # Method for computing the document idf term weighting 
     def compute_idf(self, term: str):
         return math.log(self.num_docs / len(self.index[term]))
     
+    # Method for computing the document tfidf term weighting
     def compute_tf_idf(self, term: str, doc_id: int):
         return self.index[term][doc_id] * self.compute_idf(term)
 
+    # Method for deciding the document term weighting
     def get_weighting(self, term: str, doc_id: int):
         if self.term_weighting == "binary":
             return 1
